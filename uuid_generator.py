ed83e92c9b58538d76b55e7973b4b80690182071
Karthik Suresh <karthiksuresh324@gmail.com>
2024-04-16 03:52:37 +0000

    Latest changes, Moved Java solns, increased uuid gen length, other changes

--- uuid_generator.py
+++ uuid_generator.py
@@ -6,7 +6,7 @@ uuid_path = 'testing/uuids.pkl'
 with open(uuid_path, 'rb') as file:
     uuids = pickle.load(file)
 
-def uuid_generator(length=4):
+def uuid_generator(length=5):
     characters = string.ascii_letters + string.digits
     while True:
         u = ''.join(random.choices(characters, k=length))
